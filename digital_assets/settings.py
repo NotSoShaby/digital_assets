f5274fe19e09292a7f64a84aedeead91e8dbd372
refael <refael@tailor-ip.com>
2022-01-12 01:19:57 +0200

    staticfiles

--- digital_assets/settings.py
+++ digital_assets/settings.py
@@ -124,26 +124,19 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-# STATIC_URL = '/static/'
-#
-#
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'reactapp/build/static'),
-# ]
-
-STATIC_URL = 'https://django-main.s3.amazonaws.com/'
-ADMIN_MEDIA_PREFIX = 'https://django-main.s3.amazonaws.com/admin/'
-
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_S3_REGION_NAME = 'us-east-2'
+AWS_STORAGE_BUCKET_NAME = 'django-main-1'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
 
-# AWS_ACCESS_KEY_ID = ''
-# AWS_SECRET_ACCESS_KEY = ''
-AWS_STORAGE_BUCKET_NAME = 'django-main'
+
+STATIC_URL = 'https://django-main.s3.amazonaws.com/'
+ADMIN_MEDIA_PREFIX = 'https://django-main.s3.amazonaws.com/admin/'
+
+
 
 
